49242c6bec3a5dac3a0cf89d54394dad0ad42fb8
Matthew Stephens <stephens999@gmail.com>
2020-10-31 10:33:14 -0500

    modify test

--- tests/testthat/test_k_elbo_increasing.R
+++ tests/testthat/test_k_elbo_increasing.R
@@ -8,8 +8,8 @@ test_that("elbo is increasing with k option", {
   X = matrix(rnorm(n*p),ncol = p)
   btrue = rnorm(p)
   y = drop(X %*% btrue + sd*rnorm(n))
-  fit.ebmr = ebmr(X,y,tol = 1e-10,maxiter = 100,admm = TRUE,k = 10)
+  suppressWarnings(fit.ebmr <- ebmr(X,y,tol = 1e-10,maxiter = 100,admm = TRUE,k = 10))
 
   # Check non-decreasing with tolerance.
-  expect_true(all(diff(fit.ebmr$elbo) >= -1e8)) 
+  expect_true(all(diff(fit.ebmr$elbo) >= -1e8))
 })
